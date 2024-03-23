56761c883c4fe7e9d61e97fd0098884c730bc250
Jenna Pollack <jennap3@umbc.edu>
2024-03-23 16:08:44 -0400

    fixed vuetify imports, added home page and recipe book page, added incorrect code to connect buttons to pages but I'll fix the button

--- cookheads/src/App.vue
+++ cookheads/src/App.vue
@@ -1,17 +1,50 @@
-<template>
+<!-- <template>
   <img alt="Vue logo" src="./assets/logo.png">
   <HelloWorld msg="Welcome CookHeads to Your Vue.js App"/>
+</template> -->
+<template>
+  <v-app>
+    <v-container>
+      <!-- <v-icon icon="mdi-home" /> -->
+
+      <v-app-bar style="background-color: blueviolet;" color="teal-darken-4"  :elevation="2">
+        <template v-slot:prepend>
+          <v-app-bar-nav-icon style="color:red"></v-app-bar-nav-icon>
+        </template>
+        <v-btn style="background-color:blueviolet;color:white;font-weight: bolder;" onclick={setView(1)}>Home</v-btn>
+        <v-btn style="background-color:blueviolet;color:white;font-weight: bolder;" onclick={setView(2)}>Recipes</v-btn>
+
+        <!-- <v-icon>mdi-heart</v-icon> -->
+        <v-icon icon="mdi-home" />
+        <v-app-bar-title>Cookheads Recipe Project (coming soon!)</v-app-bar-title>
+      </v-app-bar>
+      <h1>hi</h1>
+      <!-- <v-icon icon="mdi-home" /> -->
+      <HomePage show={viewPage==1}></HomePage>
+      <HomePage show={viewPage==2}></HomePage>
+      <RecipeBook></RecipeBook>
+    </v-container>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+import HomePage from './components/HomePage.vue'
+import RecipeBook from './components/RecipeBook.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HomePage,
+    RecipeBook
   }
 }
+
+let viewPage = 1;  //homepage
+function setView(pageNum) {
+   viewPage = pageNum;
+}
+
 </script>
 
 <style>
--- cookheads/src/main.js
+++ cookheads/src/main.js
@@ -1,4 +1,30 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+import { aliases, mdi } from 'vuetify/iconsets/mdi'
+
+//import Vue from 'vue'
+//import Vuetify from 'vuetify'
+//import 'vuetify/dist/vuetify.min.css' // Ensure you are using css-loader
+//@import '~vuetify/src/stylus/main' // Ensure you are using stylus-loader
+// Vuetify
+//import '@mdi/font/css/materialdesignicons.css'
+
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
+const vuetify = createVuetify({
+  components,
+  directives,
+  icons: {
+    defaultSet: 'mdi',
+    aliases,
+    sets: {
+        mdi
+    }
+  }
+})
+//Vue.use(Vuetify)
+createApp(App).use(vuetify).mount('#app')
