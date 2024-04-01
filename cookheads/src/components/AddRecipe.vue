4ad9685a34c94b59869fad4cf4c075c2dfb66d5c
John Goco <jgoco1@umbc.edu>
2024-04-01 13:00:27 -0400

    This adds the recipe for key lime pie, the correct substitutions list, and an INCORRECT version of the conversions I will fix later- mostly done in html- oops! Will fix that later too

---
+++ cookheads/src/components/AddRecipe.vue
@@ -0,0 +1,39 @@
+<template>
+    <div>
+        <v-container>
+            <v-row>
+                <v-card
+                    class="recipe-card"
+                    @click="toggleAdd()"
+                >
+                </v-card>
+
+        
+            </v-row>
+        </v-container>
+    </div>
+</template>
+
+<script>
+methods: {
+    toggleAdd() {
+
+    }
+    
+}
+</script>
+
+
+<style scoped>
+.recipe-card {
+  transition: all 0.3s ease;
+}
+.v-expand-transition-enter-active, .v-expand-transition-leave-active {
+  transition: all 0.3s ease;
+}
+.v-expand-transition-enter, .v-expand-transition-leave-to {
+  height: 0;
+  opacity: 0;
+  overflow: hidden;
+}
+</style>
