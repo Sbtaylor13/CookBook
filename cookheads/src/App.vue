8ac660f7e1c0ddc255cb57b2b8143070b8f31a6b
Jenna Pollack <jennap3@umbc.edu>
2024-04-02 18:53:08 -0400

    re commiting bc error

--- cookheads/src/App.vue
+++ cookheads/src/App.vue
@@ -16,26 +16,8 @@ export default {
   <v-app>
     <v-container>
       <RecipeCard />
-<<<<<<< HEAD
-      <v-app-bar style="background-color: blueviolet;" color="teal-darken-4"  :elevation="2">
-        <template v-slot:prepend>
-          <v-app-bar-nav-icon style="color:red"></v-app-bar-nav-icon>
-        </template>
-        <v-btn style="background-color:blueviolet;color:white;font-weight: bolder;" @click="pageView">Home</v-btn>
-        <!--  <v-btn style="background-color:blueviolet;color:white;font-weight: bolder;" @click="pageView">Recipes</v-btn>-->
 
-        <!-- <v-icon>mdi-heart</v-icon> -->
-        <v-icon icon="mdi-home" />
-        <v-app-bar-title>Cookheads Recipe Project (coming soon!)</v-app-bar-title>
-        <v-btn style="background-color:blueviolet;color:white;font-weight: bolder;" @click="pageView">+ New Recipe</v-btn>
-      </v-app-bar>
-      <!-- <v-icon icon="mdi-home" /> -->
-      <!-- <HomePage></HomePage> 
-      <HomePage v-show="viewPage===1"></HomePage>
-      <RecipeBook  v-show="viewPage===2"></RecipeBook>-->
-=======
       <TopNavbar />
->>>>>>> 6bb2659963be4c86645a26ba21a02766d729bd10
     </v-container>
   </v-app>
 </template>
